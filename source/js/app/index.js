231695e25b0ae34b06be2c8b046327707bae1ada
Emir <emirdelictbf@gmail.com>
2020-04-05 13:19:00 +0200

    Clean working version

--- source/js/app/index.js
+++ source/js/app/index.js
@@ -1,8 +1,8 @@
 /** Import Scss so it can go through webpack */
-import "./css/index.scss";
+import './css/index.scss';
 /** Init other app */
 
-import { test } from "./js/start";
+import { test } from './js/start';
 const init = () => {
     test();
 };
--- source/js/app/js/start.js
+++ source/js/app/js/start.js
@@ -1,11 +1,11 @@
 const test = () => {
-    console.log(">>>>>>>>>>>>>>>> this is testing");
-    const $div = document.createElement("div");
-    const $p = document.createElement("p");
-    $div.innerHTML = "Testing Application";
-    $p.innerHTML = "Testing class";
-    $p.classList.add("test");
-    const $body = document.getElementById("ecx-main");
+    console.log('>>>>>>>>>>>>>>>> this is testing');
+    const $div = document.createElement('div');
+    const $p = document.createElement('p');
+    $div.innerHTML = 'Testing Application';
+    $p.innerHTML = 'Testing class';
+    $p.classList.add('test');
+    const $body = document.getElementById('ecx-main');
     $body.appendChild($div);
     $body.appendChild($p);
 };
